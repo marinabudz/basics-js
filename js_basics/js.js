2dfc0d27b25430d6ad805f733476e5887acfb818
marina <budz.marina97@gmail.com>
2020-04-22 12:35:42 +0300

    upload files
    upload files

---
+++ js_basics/js.js
@@ -0,0 +1,13 @@
+// alert("wow that's anotheer file")
+
+// alert("Hello world!");
+// let message1 = 'Я люблю фронт енд';
+// let message2 = 'Я люблю JS';
+// console.log(message1);
+// console.log(message2);
+// let name = 'Marina';
+// let admin = undefined;
+// admin = name;
+// alert(admin);
+// let userName = prompt("Please insert your name");
+// alert(`${userName} Батькович`);
